0b9e8f038b878e8804a48e13bcc3e036b1ce6afa
LaoZhaoyi <1710582041@qq.com>
2026-07-15 14:30:44 +0800

    添加安全的账号配置占位文件

---
+++ sysu-course-selector/info.py
@@ -0,0 +1,8 @@
+"""Local account template.
+
+Copy these values to info.py and replace them with your own credentials.
+Do not commit real credentials.
+"""
+
+name = 'YOUR_NET_ID'
+pwd = 'YOUR_PASSWORD'
